7e3dcb13ee68b8e96f3d1817187312dcc7e17e45
Sam Szuflita <sam.szuflita@gmail.com>
2014-05-30 12:48:34 -0700

    Draw graph for line on click

--- maps.js
+++ maps.js
@@ -211,6 +211,53 @@ function TimeControl(controlDiv) {
     return controlText;
 }
 
+function generateChart(line) {
+    var dataSource = [
+	{ year: 1950, europe: 546, americas: 332, africa: 227 },
+	{ year: 1960, europe: 605, americas: 417, africa: 283 },
+	{ year: 1970, europe: 656, americas: 513, africa: 361 },
+	{ year: 1980, europe: 694, americas: 614, africa: 471 },
+	{ year: 1990, europe: 721, americas: 721, africa: 623 },
+	{ year: 2000, europe: 730, americas: 836, africa: 797 },
+	{ year: 2010, europe: 728, americas: 935, africa: 982 },
+	{ year: 2020, europe: 721, americas: 1027, africa: 1189 },
+	{ year: 2030, europe: 704, americas: 1110, africa: 1416 },
+	{ year: 2040, europe: 680, americas: 1178, africa: 1665 },
+	{ year: 2050, europe: 650, americas: 1231, africa: 1937 }
+    ];
+
+    return {
+	dataSource: dataSource,
+	commonSeriesSettings: {
+	    argumentField: "year"
+	},
+	series: [
+	    { valueField: "europe", name: "Europe" },
+	    { valueField: "americas", name: "Americas" },
+	    { valueField: "africa", name: "Africa" }
+	],
+	argumentAxis:{
+	    grid:{
+		visible: true
+	    }
+	},
+	tooltip:{
+	    enabled: true
+	},
+	title: "Power",
+	legend: {
+	    verticalAlignment: "bottom",
+	    horizontalAlignment: "center"
+	},
+	commonPaneSettings: {
+	    border:{
+		visible: true,
+		right: false
+	    }       
+	}
+    }
+}
+
 function initialize() {
     var ctx = $("#lineChart");
 
@@ -354,51 +401,10 @@ function initialize() {
 	    
 	    google.maps.event.addListener(polyLinePath, 'click', function() {
 		ctx.css('display', 'inline');
-		
-		var dataSource = [
-		    { year: 1950, europe: 546, americas: 332, africa: 227 },
-		    { year: 1960, europe: 605, americas: 417, africa: 283 },
-		    { year: 1970, europe: 656, americas: 513, africa: 361 },
-		    { year: 1980, europe: 694, americas: 614, africa: 471 },
-		    { year: 1990, europe: 721, americas: 721, africa: 623 },
-		    { year: 2000, europe: 730, americas: 836, africa: 797 },
-		    { year: 2010, europe: 728, americas: 935, africa: 982 },
-		    { year: 2020, europe: 721, americas: 1027, africa: 1189 },
-		    { year: 2030, europe: 704, americas: 1110, africa: 1416 },
-		    { year: 2040, europe: 680, americas: 1178, africa: 1665 },
-		    { year: 2050, europe: 650, americas: 1231, africa: 1937 }
-		];
+		console.log(lines);
 
-		ctx.dxChart({
-		    dataSource: dataSource,
-		    commonSeriesSettings: {
-			argumentField: "year"
-		    },
-		    series: [
-			{ valueField: "europe", name: "Europe" },
-			{ valueField: "americas", name: "Americas" },
-			{ valueField: "africa", name: "Africa" }
-		    ],
-		    argumentAxis:{
-			grid:{
-			    visible: true
-			}
-		    },
-		    tooltip:{
-			enabled: true
-		    },
-		    title: "Historic, Current and Future Population",
-		    legend: {
-			verticalAlignment: "bottom",
-			horizontalAlignment: "center"
-		    },
-		    commonPaneSettings: {
-			border:{
-			    visible: true,
-			    right: false
-			}       
-		    }
-		});
+		/*var chart = generateChart(line);
+		ctx.dxChart(chart);*/
 		
 	    });
             
