dd1197135fe8d96429904c87eed5c8b2d2a64057
Sam Szuflita <sam.szuflita@gmail.com>
2014-05-30 12:19:24 -0700

    Draw graph for line on click

--- maps.js
+++ maps.js
@@ -354,111 +354,52 @@ function initialize() {
 	    
 	    google.maps.event.addListener(polyLinePath, 'click', function() {
 		ctx.css('display', 'inline');
-		var data = {
-		    labels : ["January","February",
-			      "March","April","May","June","July"],
-		    datasets : [
-			{
-			    fillColor : "rgba(220,220,220,0.5)",
-			    strokeColor : "rgba(220,220,220,1)",
-			    pointColor : "rgba(220,220,220,1)",
-			    pointStrokeColor : "#fff",
-			    data : [65,59,90,81,56,55,40]
-			},
-			{
-			    fillColor : "rgba(151,187,205,0.5)",
-			    strokeColor : "rgba(151,187,205,1)",
-			    pointColor : "rgba(151,187,205,1)",
-			    pointStrokeColor : "#fff",
-			    data : [28,48,40,19,96,27,100]
+		
+		var dataSource = [
+		    { year: 1950, europe: 546, americas: 332, africa: 227 },
+		    { year: 1960, europe: 605, americas: 417, africa: 283 },
+		    { year: 1970, europe: 656, americas: 513, africa: 361 },
+		    { year: 1980, europe: 694, americas: 614, africa: 471 },
+		    { year: 1990, europe: 721, americas: 721, africa: 623 },
+		    { year: 2000, europe: 730, americas: 836, africa: 797 },
+		    { year: 2010, europe: 728, americas: 935, africa: 982 },
+		    { year: 2020, europe: 721, americas: 1027, africa: 1189 },
+		    { year: 2030, europe: 704, americas: 1110, africa: 1416 },
+		    { year: 2040, europe: 680, americas: 1178, africa: 1665 },
+		    { year: 2050, europe: 650, americas: 1231, africa: 1937 }
+		];
+
+		$("#chartContainer").dxChart({
+		    dataSource: dataSource,
+		    commonSeriesSettings: {
+			argumentField: "year"
+		    },
+		    series: [
+			{ valueField: "europe", name: "Europe" },
+			{ valueField: "americas", name: "Americas" },
+			{ valueField: "africa", name: "Africa" }
+		    ],
+		    argumentAxis:{
+			grid:{
+			    visible: true
 			}
-		    ]
-		};
-
-		Line.defaults = {
-		    
-		    //Boolean - If we show the scale above the chart data
-		    scaleOverlay : false,
-		    
-		    //Boolean - If we want to override with a hard coded scale
-		    scaleOverride : false,
-		    
-		    //** Required if scaleOverride is true **
-		    //Number - The number of steps in a hard coded scale
-		    scaleSteps : null,
-		    //Number - The value jump in the hard coded scale
-		    scaleStepWidth : null,
-		    //Number - The scale starting value
-		    scaleStartValue : null,
-
-		    //String - Colour of the scale line
-		    scaleLineColor : "rgba(0,0,0,.1)",
-		    
-		    //Number - Pixel width of the scale line
-		    scaleLineWidth : 1,
-
-		    //Boolean - Whether to show labels on the scale
-		    scaleShowLabels : true,
-		    
-		    //Interpolated JS string - can access value
-		    scaleLabel : "<%=value%>",
-		    
-		    //String - Scale label font declaration for the scale label
-		    scaleFontFamily : "'Arial'",
-		    
-		    //Number - Scale label font size in pixels
-		    scaleFontSize : 12,
-		    
-		    //String - Scale label font weight style
-		    scaleFontStyle : "normal",
-		    
-		    //String - Scale label font colour
-		    scaleFontColor : "#666",
-		    
-		    ///Boolean - Whether grid lines are shown across the chart
-		    scaleShowGridLines : true,
-		    
-		    //String - Colour of the grid lines
-		    scaleGridLineColor : "rgba(0,0,0,.05)",
-		    
-		    //Number - Width of the grid lines
-		    scaleGridLineWidth : 1,
-		    
-		    //Boolean - Whether the line is curved between points
-		    bezierCurve : true,
-		    
-		    //Boolean - Whether to show a dot for each point
-		    pointDot : true,
-		    
-		    //Number - Radius of each point dot in pixels
-		    pointDotRadius : 3,
-		    
-		    //Number - Pixel width of point dot stroke
-		    pointDotStrokeWidth : 1,
-		    
-		    //Boolean - Whether to show a stroke for datasets
-		    datasetStroke : true,
-		    
-		    //Number - Pixel width of dataset stroke
-		    datasetStrokeWidth : 2,
-		    
-		    //Boolean - Whether to fill the dataset with a colour
-		    datasetFill : true,
-		    
-		    //Boolean - Whether to animate the chart
-		    animation : true,
-
-		    //Number - Number of animation steps
-		    animationSteps : 60,
-		    
-		    //String - Animation easing effect
-		    animationEasing : "easeOutQuart",
-
-		    //Function - Fires when the animation is complete
-		    onAnimationComplete : null
-		    
-		};
-		new Chart(ctx).Line(data,options);
+		    },
+		    tooltip:{
+			enabled: true
+		    },
+		    title: "Historic, Current and Future Population",
+		    legend: {
+			verticalAlignment: "bottom",
+			horizontalAlignment: "center"
+		    },
+		    commonPaneSettings: {
+			border:{
+			    visible: true,
+			    right: false
+			}       
+		    }
+		});
+		
 	    });
             
             // Cache polylines 
